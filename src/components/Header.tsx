f2697716fff0cdb5cbbbef7c89e881e1d063e588
Ivan-Eliseev <mrkatyt@gmail.com>
2022-06-23 20:24:46 +0300

    Commit - 1.1

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/2/21/VK.com-logo.svg'/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Profile.css';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://www.vzsar.ru/i/news/xxl/2021/07/221871_1626341880.jpg'/>
+            </div>
+
+            <div>ava+description</div>
+            <div>My posts</div>
+            <div>New post</div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+
+        </div>
+    );
+};
+
+export default Profile;
